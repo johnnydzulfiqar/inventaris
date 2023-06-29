e98881b4a8832170b23eb2fc10bf917d20ec7a92
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2023-06-29 23:44:50 +0700

    last update

--- resources/views/guru/indexkeluar.blade.php
+++ resources/views/guru/indexkeluar.blade.php
@@ -152,5 +152,46 @@ Index User
 
       </div>
   </div>
-
+  <script>
+    $(document).ready(function() {
+              $('#table_id').DataTable({
+  
+                  dom: 'Bfrtip',
+                  responsive: true,
+                  pageLength: 25,
+                  // lengthMenu: [0, 5, 10, 20, 50, 100, 200, 500],
+  
+                  buttons: [
+                      {
+                  extend: 'copyHtml5',
+                  exportOptions: {
+                      columns: [ 0, 1, 2, 3, 4, 5, 6, ]
+                  }
+              }, {
+                  extend: 'csvHtml5',
+                  exportOptions: {
+                      columns: [ 0, 1, 2, 3, 4, 5, 6, ]
+                  }
+              },  {
+                  extend: 'excelHtml5',
+                  exportOptions: {
+                      columns: [ 0, 1, 2, 3, 4, 5, 6, ]
+                  }
+              }, {
+                  extend: 'pdfHtml5',
+                  exportOptions: {
+                      columns: [ 0, 1, 2, 3, 4, 5, 6, ]
+                  }
+              }, {
+                  extend: 'print',
+                  exportOptions: {
+                      columns: [ 0, 1, 2, 3, 4, 5, 6, ]
+                  }
+              },
+                  ]
+  
+              });
+              
+          });
+  </script>
 @endsection
